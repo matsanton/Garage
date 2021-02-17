5306129e5a4945d5976c8ed95e8a1300f99fec5e
Mats Antonsson <matan848@hotmail.com>
2021-02-17 17:28:40 +0100

    Add Leave method

--- Garage/Garage.cs
+++ Garage/Garage.cs
@@ -3,18 +3,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GarageApp
 {
     public class Garage<T> : IEnumerable<T> where T : Vehicle
     {
+        #region Private fields
         private readonly T[] vehicles;
         private int count = 0;
+        #endregion
 
+        #region Properties
         public int Capacity { get; }
-        public bool SpaceAvailable { get; private set; } = true;
+        public bool IsFreeSpace { get; private set; } = true;
         public int Count => count;
+        #endregion
 
+        #region Constructor
         public Garage(int capacity)
         {
             if (capacity <= 0)
@@ -24,33 +30,68 @@ namespace GarageApp
             Capacity = capacity;
             vehicles = new T[capacity];
         }
+        #endregion
 
-        internal bool Park(T vehicle)
+        #region Public Methods
+        public bool Park(T vehicle)
         {
-            if (SpaceAvailable)
+            if (IsFreeSpace)
             {
-                vehicles[count++] = vehicle;
-                if (count == Capacity)
+                if (GetFirstFreeSpot(out int index))
                 {
-                    SpaceAvailable = false;
+                    count++;
+                    vehicles[index] = vehicle;
+                    if (count == Capacity) IsFreeSpace = false;
+                    return true;
                 }
-                return true;
             }
             return false;
         }
 
+        private bool GetFirstFreeSpot(out int index)
+        {
+            index = 0;
+            for (int i = 0; i < vehicles.Length; i++)
+            {
+                if (vehicles[i] == null)
+                {
+                    index = i;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Leave(T curentVehicle)
+        {
+            if (curentVehicle == null) return false;
+            int index = Array.IndexOf(vehicles, curentVehicle);
+
+            if (index >= 0)
+            {
+                // The vehicle is in the garage
+                vehicles[index] = null;
+                count--;
+                IsFreeSpace = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        #endregion
         public IEnumerator<T> GetEnumerator()
         {
-            foreach(var item in vehicles)
+            foreach (var item in vehicles)
             {
                 yield return item as T;
             }
         }
-
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
-       
     }
 }
